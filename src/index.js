643f97ba59f25b82c9875f2327fc9fee9e1e1088
viktorfa <vikfand@gmail.com>
2018-11-09 12:52:21 +0100

    Install semantic ui

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import { Provider } from 'react-redux'
 import { createStore, combineReducers, applyMiddleware } from 'redux'
 import thunk from 'redux-thunk'
 import { composeWithDevTools } from 'redux-devtools-extension';
+import 'semantic-ui-css/semantic.min.css';
 
 import './index.css';
 import App from './App';
